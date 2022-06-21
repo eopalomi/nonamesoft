e8b1a032a60e933c021361e2a63e933a89b7000a
Erick <eopalomi@gmail.com>
2022-06-21 07:40:13 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { MainModule } from './main/main.module';
+import { SideMenuModule } from './side-menu/side-menu.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    MainModule,
+    SideMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
